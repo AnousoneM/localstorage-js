a4b65be2c60b63e7432f958e7ba493c3759e1e85
anousonem <anousone.mounivongs@gmail.com>
2022-12-05 20:57:01 +0100

    mise à jour

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,8 +47,31 @@ window.addEventListener('click', event => {
 })
 
 document.querySelector('#add-button').addEventListener('click', (e) => {
-    // pour éviter de submit le formulaire
+    // pour éviter de submit le formulaire, nous faisons un preventDefault
     e.preventDefault()
 
-    
+    // nous effectuons une condition pour ne pas permettre d'enregistrer du vide
+    if (document.querySelector('input[name="myNote"]').value != '') {
+        // on crée un élément div avec ses classes 
+        const newDiv = document.createElement('div')
+        newDiv.classList.add('d-table-cell', 'align-middle', 'mb-1', 'border')
+
+        // utilisation de innerHTML pour ne pas alourdir la syntaxe en raison des multiples attributs : classe, data, type, etc ... 
+        newDiv.innerHTML = `
+            <input data-old-value="${document.querySelector('input[name="myNote"]').value}" data-color="${document.querySelector('select[name="myColor"]').value}" type="text" class="col-lg-4 fw-bold btn btn-${document.querySelector('select[name="myColor"]').value} shadow p-2"
+            value="${document.querySelector('input[name="myNote"]').value}" disabled>
+            <i data-action="edit" class="bi bi-pencil-fill btn btn-outline-secondary"></i>
+            <i data-action="remove" class="bi bi-trash-fill btn btn-outline-danger"></i>
+            <i data-action="valid" class="bi bi-check-lg btn btn-outline-success" style="display:none"></i>
+            <i data-action="cancel" class="bi bi bi-x-lg btn btn-outline-secondary" style="display:none"></i>
+        `
+
+        // utilisation de appendChild pour ajouter l'élément dans la div 
+        document.querySelector('#allNotes').appendChild(newDiv)
+
+        // on efface l'input
+        document.querySelector('input[name="myNote"]').value = ''
+    }
 })
+
+
